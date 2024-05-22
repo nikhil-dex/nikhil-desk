f5b7f0de13f3d6c28d819cc1166a4362b6a1b941
nikhil-dex <167549127+nikhil-dex@users.noreply.github.com>
2024-05-22 19:57:35 +0530

    Add files via upload

--- scripts/body.js
+++ scripts/body.js
@@ -1,11 +1,3 @@
-document.body.style.backgroundColor = 'white';
-let Time = new Date();
-if (Time.getHours() < 15) {
-    document.body.style.backgroundColor = 'white';
-} else{
-    document.body.style.backgroundColor = 'black';
-
-}
 function changeBackgroundColor() {
     document.body.style.backgroundColor = (document.body.style.backgroundColor=='white') ? 'black': 'white';
     document.querySelector("#loader").style.background = (document.body.style.backgroundColor=='white') ? 'black': 'white';
@@ -15,6 +7,38 @@ function changeBackgroundColor() {
     document.documentElement.style.setProperty('--bblack', (document.body.style.backgroundColor=='white') ? 'white': '#333');
     document.documentElement.style.setProperty('--bwriten', (document.body.style.backgroundColor=='white') ? 'black': 'white');
     document.documentElement.style.setProperty('--bwritenb', (document.body.style.backgroundColor=='black') ? 'white': 'black');
-
-  
 }
+document.body.style.backgroundColor = 'white';
+let Time = new Date();
+if (Time.getHours() < 19) {
+    document.body.style.backgroundColor = 'white';
+    document.documentElement.style.setProperty('--bwriten', (document.body.style.backgroundColor=='white') ? 'black': 'white');
+    document.documentElement.style.setProperty('--bblack', (document.body.style.backgroundColor=='white') ? 'white': '#333');
+    document.documentElement.style.setProperty('--bwhite', (document.body.style.backgroundColor=='white') ? 'black': 'white'); 
+    document.querySelector("#loader").style.background = (document.body.style.backgroundColor=='white') ? 'black': 'white';
+    document.documentElement.style.setProperty('--bwritenb', (document.body.style.backgroundColor=='white') ? 'black': 'white');
+    
+} else{
+    document.body.style.backgroundColor = 'black';
+    document.documentElement.style.setProperty('--bwritenb', (document.body.style.backgroundColor=='black') ? 'white': 'black');
+
+}
+
+// scroll animation for mozilla
+// document.addEventListener('DOMContentLoaded', ()=> {
+//     const observerOptions = {
+//         threshold: 0.1
+//     };
+//     const observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add('visible');
+//                 observer.unobserve(entry.target);
+//             }
+//             });
+//         }, observerOptions);
+//         document.querySelectorAll('#containerA').forEach((element) => {
+//             observer.observe(element);
+//         });
+    
+// });
